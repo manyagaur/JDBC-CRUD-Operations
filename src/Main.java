cf18a54896303e894b7ed3825366779130270f81
manyagaur <manyagaur25@gmail.com>
2024-02-08 15:49:23 +0530

    Added insert operation

--- src/Main.java
+++ src/Main.java
@@ -10,23 +10,46 @@ public class Main {
 
     public static void main(String[] args) {
         try {
-            //Read Operation
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException ex) {
             System.out.println(ex.getMessage());
         }
+        /*
         try {
             Connection connection = DriverManager.getConnection(url, user, password);
+
+            //Read Operation
             String query = "select * from Trains";
             Statement statement = connection.createStatement();
             ResultSet result = statement.executeQuery(query);
             while (result.next()) {
                 String name = result.getString("name");
                 System.out.println(name);
+
+
             }
         } catch (SQLException ex) {
             System.out.println(ex.getMessage());
+        } */
+
+        try{
+            Connection connection=DriverManager.getConnection(url,user,password);
+            Statement statement=connection.createStatement();
+            String query=String.format("insert into Trains(name, day_of_arrival, time_of_arrival)"+ "values('%s','%s','%s')",  "Porbandar Express", "Thursday", "5:00");
+            int rows_affected=statement.executeUpdate(query);
+            if (rows_affected>0){
+                System.out.println(rows_affected);
+            }
+
+
+
+
         }
+        catch(SQLException ex){
+            System.out.println(ex.getMessage());
+        }
+
+
 
     }
 }
